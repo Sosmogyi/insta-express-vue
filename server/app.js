9d3b0fa1e804cb1144d4445f97780d862ac5f8e6
Sosmogyi <nexus.laci@gmail.com>
2018-08-04 16:23:00 +0200

    base url & express static

--- server/app.js
+++ server/app.js
@@ -3,15 +3,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const Sequelize = require('sequelize')
 const {sequelize} = require('./src/models') // database models
 const config = require('./src/config/config') // portconfig
+const path = require('path')
 
 const app = express()
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, '../../client/dist')))
 
 require('./src/routes')(app)
 
